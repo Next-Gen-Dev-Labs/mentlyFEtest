5cdeb0063dee3b53c0ad290230598c2d05a791cc
Ikenna Richard <oguejioforrichard@gmail.com>
2025-02-07 12:19:08 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Chivo } from "next/font/google";
+import "./globals.css";
+
+const chivo = Chivo({
+  variable: "--font-chivo",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Mently",
+  description: "Easily host effective training and mentoring programs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${chivo.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-chivo)]">
+     
+    </div>
+  );
+}
