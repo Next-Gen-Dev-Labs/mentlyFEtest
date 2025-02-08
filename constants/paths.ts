48eec853e7cfd62d6e712c63227c4684f357a6da
whyte25 <fasifeoluwa3@gmail.com>
2025-02-08 19:13:16 +0100

    chore: setup of ui components, images,  icons, pretter and vitest

---
+++ constants/paths.ts
@@ -0,0 +1,17 @@
+export const paths = {
+  auth: {
+    login: "/login",
+  },
+  private: {
+    home: "#",
+    programs: "/dashboard/programs",
+    activities: "#",
+    forms: "#",
+    finances: "#",
+    badges: "#",
+    analytics: "#",
+    settings: "#",
+  },
+};
+
+export type PathsTypes = typeof paths;
---
+++ constants/site-config.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  title: "Mently",
+  name: "Mently",
+  url: "https://scrollz-web.netlify.app/",
+  author: "ifeoluw fas",
+  ogImage: "/og-image.png",
+  description:
+    "Accelerate Career Growth and Knowledge Sharing Through Mentoring",
+  keywords: ["Mentorship", "Career Growth", "Knowledge Sharing"],
+  links: {
+    twitter: "https://x.com/mymentlyhq",
+  },
+};
+
+export type SiteConfigTypes = typeof siteConfig;
