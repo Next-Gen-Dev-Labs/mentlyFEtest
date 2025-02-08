eabeeac22b581c7886e0da801bc76f74ee723572
Ikenna Richard <oguejioforrichard@gmail.com>
2025-02-08 13:44:49 +0100

    feat: add dashboard header

---
+++ components/header/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import NotificationBell from "../NotificationBell";
+import Image from "next/image";
+import avatar from "@/public/avatar.png";
+import Link from "next/link";
+import { ArrowDown2 } from "iconsax-react";
+
+export default function Header() {
+  return (
+    <header className="bg-header flex justify-end p-3 shadow-sm">
+      <div className="flex gap-6 items-center mr-[6em]">
+        <NotificationBell />
+        <div className="flex gap-3 items-center justify-start max-w-56">
+          <div className="w-12 h-12 bg-white relative rounded-full overflow-hidden">
+            <Image src={avatar} fill alt="headshot of godwin jimmy" />
+          </div>
+      
+            <div className="flex flex-col">
+              <p className="text-sm text-black">Godwin Jimmy</p>
+              <span className="flex items-baseline gap-2">
+                <small className="text-gray-500 text-xs">Free Plan</small>
+                <Link
+                  href="#"
+                  className="font-bold text-sm text-mently-link-color"
+                >
+                  Upgrade
+                </Link>
+              </span>
+            </div>
+          <button className="appearance-none">
+            <ArrowDown2 size={16} variant="Linear" className="stroke-icon-grey" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
