fe99a43d3b284a2b58b59e36d2c66049c5d8dea1
MayDan12 <oluwamayowadan@gmail.com>
2025-02-09 00:19:56 +0100

    Fixed somethings

--- components/ProgramInfo.jsx
+++ components/ProgramInfo.jsx
@@ -3,7 +3,7 @@ import ProgramPreview from "./ProgramPreview";
 
 export default function ProgramPage() {
   return (
-    <div className="min-h-screen bg-gray-50 p-6 mt-16">
+    <div className="min-h-screen bg-white p-6 mt-16">
       <div className="grid md:grid-cols-2 gap-8">
         <ProgramEditor />
         <ProgramPreview />
