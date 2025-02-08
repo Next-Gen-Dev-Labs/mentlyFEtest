3a0e00efe1456d8feb4c87048ceeb857c1b34cf1
Ikenna Richard <oguejioforrichard@gmail.com>
2025-02-08 23:37:35 +0100

    update configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,108 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        chivo: ["var(--font-chivo)", "sans-serif"]
-      },
-      colors: {
-        primary: "#1F0954",
-      },
-    },
+  	extend: {
+			screens: {
+				'x-lg': '1157px',
+				'x-md': '1046px',
+				'x-sm': '912px',
+			},
+  		fontFamily: {
+  			chivo: [
+  				'var(--font-chivo)',
+  				'sans-serif'
+  			]
+  		},
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(circle, red 0%, red 400px, black 100px, black 100%)'
+  		},
+  		colors: {
+  			'mently-blue': '#1F0954',
+  			'icon-grey': '#aaaaaa',
+  			'mently-honey': '#F0C074',
+  			'icon-honey': '#FFECCC',
+  			'header': '#FDFDFD',
+  			'gradient-pink': '#FF0044',
+  			'gradient-yellow': '#F6A000',
+  			'mently-purple': '#8C00E3',
+				'mently-link-color': '#2B85FE',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [animate],
 } satisfies Config;
