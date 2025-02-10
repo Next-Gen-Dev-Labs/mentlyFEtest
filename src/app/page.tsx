e14e9e46ed0925e52ba4c908e8e97c6eb673241d
Ebun22 <subeydavid22@gmail.com>
2025-02-10 11:52:53 +0100

    added darkmode to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link'
+import { useAppContext } from './context/AppContext';
 
 export default function Home() {
+  const { darktheme, setDarkTheme } = useAppContext();
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen'>
+    <div className={`flex flex-col items-center justify-center min-h-screen ${darktheme ? "bg-appDeepTextBlue text-white" : "bg-[#FDFDFD]"}`}>
       <p className=''>This is home.
       </p>
-      <Link href="/programs" className='cursor-pointer hover:underline focus:underline'>
-          Go to Programs
-        </Link>
+      <Link
+        href="/programs"
+        className='cursor-pointer hover:underline focus:underline'>
+        Go to Programs
+      </Link>
     </ div>
   )
 }
