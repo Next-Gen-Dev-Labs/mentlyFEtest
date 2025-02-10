13a3af96e469ead579c21f0e141667dd1d80e320
Ebun22 <subeydavid22@gmail.com>
2025-02-10 11:41:40 +0100

    addeed aria-label to toggle to imporve accessibility

--- src/app/components/ui/ToggleSwitch.tsx
+++ src/app/components/ui/ToggleSwitch.tsx
@@ -6,27 +6,40 @@ const ToggleSwitch = () => {
 
     return (
         <>
-            <label className="flex items-center justify-center relative gap-x-2 mt-10 cursor-pointer">
+            <label
+                htmlFor="dark-mode-toggle"
+                className="flex items-center justify-center relative gap-x-2 mt-10 cursor-pointer"
+            >
                 {/* Hidden Checkbox Input */}
                 <input
+                    id="dark-mode-toggle"
                     value={"on"}
                     type="checkbox"
                     className="hidden"
                     checked={darktheme}
                     onChange={() => setDarkTheme(!darktheme)}
+                    aria-label="Toggle dark mode"
                 />
                 {/* Toggle Background */}
-                <div className={`w-10 h-5 rounded-full transition-colors duration-500 ${darktheme ? "bg-appDeepTextBlue" : "bg-[#F6F6F6]"} `}
+                <div
+                    role="switch"
+                    aria-checked={darktheme}
+                    className={`w-10 h-5 rounded-full transition-colors duration-500 ${
+                        darktheme ? "bg-appDeepTextBlue" : "bg-[#F6F6F6]"
+                    }`}
                 >
                     {/* Toggle Knob */}
                     <div
-                        className={`p-2 ml-[0.8px] ${darktheme ? "bg-white translate-x-5  transition-all" : "translate-x-0 bg-mentlyBlue  transition-all"}  rounded-full transform absolute bottom-[1.8px]`}
+                        className={`p-2 ml-[0.8px] ${
+                            darktheme
+                                ? "bg-white translate-x-5 transition-all"
+                                : "translate-x-0 bg-mentlyBlue transition-all"
+                        } rounded-full transform absolute bottom-[1.8px]`}
                     ></div>
                 </div>
-            </label >
+            </label>
         </>
     );
-}
+};
 
 export default ToggleSwitch;
-
