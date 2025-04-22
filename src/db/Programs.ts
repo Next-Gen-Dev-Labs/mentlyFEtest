6d274152a4a4d736fdfcbd2a434613f16fbc5f85
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-04-22 21:21:18 +0100

    added db for data collection

---
+++ src/db/Programs.ts
@@ -0,0 +1,75 @@
+
+
+export const programs = [
+    {
+      "title": "Fundamentals of User interface & Experience",
+      "tag": "Bootcamp",
+      "description": "This program is a hands-on guide designed for designers who want to master color theory and confidently apply it to their designs. This practical approach",
+      "mentors": "Mentors",
+      "avatars": [
+        "mentor-avatar-1.png",
+        "mentor-avatar-2.png",
+        "mentor-avatar-3.png"
+      ],
+      "backgroundImage": "fundamentals-ui.jpg"
+    },
+    {
+      "title": "Colour Hack Practical Group Call",
+      "tag": "Group Call",
+      "description": "This program is a hands-on guide designed for designers who want to master color theory and confidently apply it to their designs. This practical approach",
+      "mentors": "Hosted By: Faith Okolo",
+      "avatar": "faith-okolo.jpg",
+      "backgroundImage": "group-call-1.jpg"
+    },
+    {
+      "title": "Colour Hack Practical Group Call",
+      "tag": "Group Call",
+      "description": "This program is a hands-on guide designed for designers who want to master color theory and confidently apply it to their designs. This practical approach",
+      "mentors": "",
+      "avatar": "",
+      "backgroundImage": "group-call-2.jpg"
+    }
+  ]
+  
+  export const GroupCalls = [
+    {
+      "status": "Ongoing",
+      "title": "Weekly Meeting - Product Demo Review with Testers",
+      "date": "Mon. Jul 30, 2024",
+      "time": "9:00 - 11:00AM",
+      "studyGroup": "UX Strategy Study group",
+      "mentors": "Mentors",
+      "avatars": [
+        "mentor-avatar-1.png",
+        "mentor-avatar-2.png"
+      ],
+      "backgroundImage": "group-call-smile.jpg"
+    },
+    {
+      "status": "Upcoming",
+      "title": "Weekly Meeting - Product Demo Review with Testers",
+      "date": "Mon. Jul 30, 2024",
+      "time": "9:00 - 11:00AM",
+      "studyGroup": "UX Strategy Study group",
+      "mentors": "Mentors",
+      "avatars": [
+        "mentor-avatar-1.png",
+        "mentor-avatar-2.png"
+      ],
+      "backgroundImage": "group-call-wave.jpg"
+    },
+    {
+      "status": "Ongoing",
+      "title": "Weekly Meeting - Product Demo Review with Testers",
+      "date": "Mon. Jul 30, 2024",
+      "time": "9:00 -",
+      "studyGroup": "UX Strategy Study group",
+      "mentors": "Mentors",
+      "avatars": [
+        "mentor-avatar-1.png",
+        "mentor-avatar-2.png"
+      ],
+      "backgroundImage": "group-call-muuk.jpg"
+    }
+  ]
+  
