ec348d78d41aa80f1ab3d4758df8088f1472a869
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-04-22 20:24:40 +0100

    updated code for sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Sidebar() {
   return (
     <div className="bg-gray-800 text-white w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
-      <nav>
+      <nav className="flex flex-col text-start justify-center items-center">
         <Link href="/" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">
           Dashboard
         </Link>
@@ -23,3 +23,25 @@ export default function Sidebar() {
     </div>
   )
 } 
+
+
+/**
+ * finance
+ * import { IoWalletOutline } from "react-icons/io5";
+ * 
+ * user
+ * import { FaRegUser } from "react-icons/fa";
+ * 
+ * Rewards
+ * import { RiMedalFill } from "react-icons/ri";
+ * 
+ * settings
+ * import { CiSettings } from "react-icons/ci";
+ * 
+ * logout
+ * import { TbLogout2 } from "react-icons/tb";
+ * 
+ * widgets
+ * import { MdOutlineWidgets } from "react-icons/md";
+ * 
+ */
