b00143b82aa7367d638511dadf5e5a1ec362f952
Mike Adeleke <114467673+MikeAdelek@users.noreply.github.com>
2025-04-24 15:13:11 -0400

    Commit Initial

--- mentlyfe/src/pages/app.js
+++ mentlyfe/src/pages/app.js
@@ -1,18 +1,18 @@
-"use client";
-import "../app/globals.css";
-import { useEffect } from "react";
+// "use client";
+// import "../app/globals.css";
+// import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // This is where you can add any global styles or scripts
-    // For example, you can add a CSS file or a script tag here
-    if (typeof window !== "undefined") {
-      const feather = require("feather-icons");
-      feather.replace();
-    }
-  }, []);
+// function MyApp({ Component, pageProps }) {
+//   useEffect(() => {
+//     // This is where you can add any global styles or scripts
+//     // For example, you can add a CSS file or a script tag here
+//     if (typeof window !== "undefined") {
+//       const feather = require("feather-icons");
+//       feather.replace();
+//     }
+//   }, []);
 
-  return <Component {...pageProps} />;
-}
+//   return <Component {...pageProps} />;
+// }
 
-export default MyApp;
+// export default MyApp;
