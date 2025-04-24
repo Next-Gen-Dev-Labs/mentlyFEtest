d3ed247b68528abbcdc52bf6ba55713a114f7331
Mike Adeleke <114467673+MikeAdelek@users.noreply.github.com>
2025-04-24 15:23:22 -0400

    commit initial

--- mentlyfe/next.config.mjs
+++ mentlyfe/next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [], // Add any external domains you're loading images from
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  reactStrictMode: true,
+  swcMinify: true
 };
 
 export default nextConfig;
