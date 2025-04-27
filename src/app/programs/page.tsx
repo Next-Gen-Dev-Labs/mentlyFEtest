28f3e907a577aa620e94e050c2559d4066e6bb52
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-04-27 13:19:40 +0100

    Refactor GroupCall component to utilize reusable GroupCalls_widget
    - Updated the GroupCall component to render the GroupCalls_widget, enhancing modularity and reusability.
    - Adjusted the button to navigate to the forums page instead of the group calls page for improved navigation.
    - Streamlined the rendering logic by removing redundant code related to individual meeting cards.

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -4,9 +4,9 @@ import { programs } from "@/db/dataBase";
 
 export default function Programs() {
   return (
-    <div className="container mx-auto px-6 py-8">
+    <div className="container mx-auto md:px-6 px-4 py-8">
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (<ProgramWidgetCard programs={programs} />))}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (<span key={index}><ProgramWidgetCard programs={programs} /></span>))}
       </div>
     </div>
   )
--- src/components/widgets/GroupCall.tsx
+++ src/components/widgets/GroupCall.tsx
@@ -1,15 +1,11 @@
 "use client"
 import React from "react";
 import Card from "@/components/ui/Card";
-import ButtonPurple from "../ui/ButtonPurple";
-import { CalendarIcon, Clock } from "lucide-react";
-import ButtonWithBorderPurple from "../ui/ButtonWithBorderPurple";
 import { Menu } from "lucide-react";
 import { CiMenuKebab } from "react-icons/ci";
-import Image from "next/image";
-import { GroupCalls } from "@/db/dataBase";
-import { GroupCallIcon } from "@/components/svgIcons/SvgIcons";
 import { useRouter } from "next/navigation";
+import GroupCalls_widget from '@/components/ui/reusable_widgets/groupCalls_widget'
+import { GroupCalls } from "@/db/dataBase";
 
 const GroupCall = () => {
   const router = useRouter();
@@ -21,118 +17,14 @@ const GroupCall = () => {
           <h1 className="text-[#B0B0B0] text-base font-bold">Group Calls</h1>
         </div>
         <div className="flex items-center gap-4">
-          <button onClick={() => router.push('/group-calls')} className="text-[#6F01D0] cursor-pointer font-semibold text-xs">See all</button>
+          <button onClick={() => router.push('/forums')} className="text-[#6F01D0] cursor-pointer font-semibold text-xs">See all</button>
           <CiMenuKebab />
         </div>
       </div>
 
       <main className="absolute inset-x-0 bottom-0 top-[3rem] overflow-x-auto scrollbar-hide">
         <div className="flex gap-4 p-4 min-w-min">
-          {GroupCalls.map((meeting, index) => (
-            <Card
-              key={index}
-              className="p-4 bg-[#F9F7FF] min-w-[350px] max-w-[350px]"
-            >
-              <Image
-                src={meeting.backgroundImage}
-                alt="Description of the image"
-                width={500}
-                height={200}
-                className="w-full "
-              />
-              {/* Status Badge */}
-              <div className="flex items-center gap-2 my-2">
-                <span
-                  className={`inline-flex items-center px-2.5 py-1 rounded-full text-xs font-medium
-                  ${
-                    meeting.status === "Ongoing"
-                      ? "bg-[#d0eccc] text-[#1F8B01]"
-                      : " bg-[#E0DDFF] text-[#1C0AE1]"
-                  }
-                `}
-                >
-                  <span
-                    className={
-                      meeting.status === "Ongoing"
-                        ? "w-2 h-2 bg-[#1F8B01] rounded-full mr-1.5"
-                        : "w-2 h-2 bg-[#1C0AE1] rounded-full mr-1.5"
-                    }
-                  ></span>
-                  {meeting.status}
-                </span>
-              </div>
-
-              {/* Meeting Title */}
-              <h2 className="text-base border-[#D0D5DD] pb-1.5 border-b font-normal text-[#595564] mb-2">
-                {meeting.title}
-              </h2>
-
-              {/* Date and Time */}
-              <div className="flex items-center gap-6 mb-2">
-                <div className="flex items-center gap-2">
-                  <CalendarIcon className="w-4 font-medium h-4 text-[#595564]" />
-                  <span className="text-xs text-[#595564] font-medium">
-                    {meeting.date}
-                  </span>
-                </div>
-                <div className="flex items-center gap-2">
-                  <Clock className="w-4 h-4 font-medium text-[#595564]" />
-                  <span className="text-xs font-medium text-[#595564]">
-                    {meeting.time}
-                  </span>
-                </div>
-              </div>
-
-              {/* Study Group and Mentors */}
-              <div className="flex justify-between items-center mb-4">
-                <div>
-                  <p className="text-xs font-normal text-[#A195C0] ml-8">
-                    Study Group
-                  </p>
-                  <div className="flex items-center gap-3">
-                    <div className="flex items-center gap-2">
-                      <Image
-                        src={meeting.studyGroup.avatar}
-                        alt="Study Group"
-                        className="w-6 h-6 rounded-full"
-                      />
-                      <span className="text-xs font-semibold text-[#595564]">
-                        {meeting.studyGroup.name}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col items-center">
-                  <span className="text-xs -ml-7 font-normal text-[#A195C0]">
-                    Mentors
-                  </span>
-                  <div className="flex -space-x-2">
-                    {meeting.mentors.avatars.map((avatar, i) => (
-                      <Image
-                        key={i}
-                        src={avatar}
-                        alt={`Mentor ${i + 1}`}
-                        className="w-8 h-8 rounded-full border-2 border-white"
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
-
-              {/* Action Buttons */}
-              <div className="flex justify-between">
-                <ButtonWithBorderPurple
-                  className="px-2 py-2 text-[#6F01D0] text-xs rounded-md"
-                  value="View Participants"
-                />
-                <ButtonPurple
-                  className="px-4 py-2 flex items-center gap-2 rounded-md text-xs"
-                  value="Join Now"
-                  icon={<GroupCallIcon />}
-                />
-              </div>
-            </Card>
-          ))}
+          <GroupCalls_widget GroupCalls={GroupCalls} />
         </div>
       </main>
     </Card>
